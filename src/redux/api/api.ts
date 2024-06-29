148a4baf67de4cd0a5c0599630e772a3a9c9c21e
JilluRahmanJibon <jillurahmanjibon@gmail.com>
2024-06-29 10:09:00 +0600

    first commit

---
+++ src/redux/api/api.ts
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+
+ export const baseApi = createApi({
+    reducerPath: 'baseApi',
+    baseQuery: fetchBaseQuery({baseUrl:""}),
+     endpoints: (builder) => ({
+         getTodos: builder.query({
+             query: () => ({
+                 url: `/tasks`,
+                 method:'GET'
+            })
+        })
+    })
+ })
+
+ export const {useGetTodosQuery}=baseApi
